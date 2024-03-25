a14479e494bd20b58618f1f2cc905fbd46f4ff85
曾刘保.Felix <zenglb@vanke.com>
2024-03-25 15:48:33 +0800

    精简代码，主要是演示如何快速接入SDK

--- app/src/main/java/com/sjl/binocularcamera/CameraActivity.java
+++ app/src/main/java/com/sjl/binocularcamera/CameraActivity.java
@@ -49,13 +49,11 @@ public class CameraActivity extends BaseActivity {
     public static final String TAG = CameraActivity.class.getSimpleName();
 
     private CameraSurfaceView mCameraSurfaceView1, mCameraSurfaceView2;
-    private ImageView iv_rgb, iv_ir,search_result;
-    
+    private ImageView search_result;
     private TextView tips, logText;
     private LinearLayout ll_surface_layout;
     private int camera1DataMean, camera2DataMean;
     private volatile boolean rgbOrIrConfirm, camera1IsRgb;
-    private Button btn_rgb,btn_ir;
 
     private Bitmap realTimeFaceBmp;
 
@@ -76,17 +74,12 @@ public class CameraActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        
         search_result=findViewById(R.id.image_result);
         tips=findViewById(R.id.searchTips);
         logText =findViewById(R.id.log);
 
         ll_surface_layout = findViewById(R.id.ll_surface_layout);
         ll_surface_layout.removeAllViews();
-        iv_rgb = findViewById(R.id.iv_rgb);
-        iv_ir = findViewById(R.id.iv_ir);
-        btn_rgb = findViewById(R.id.btn_rgb);
-        btn_ir = findViewById(R.id.btn_ir);
     }
 
 
@@ -113,73 +106,52 @@ public class CameraActivity extends BaseActivity {
                 .setCameraFacing(CameraSurfaceView.CAMERA_USB)
               .createPreview();
 
-        mCameraSurfaceView1.setOnPreviewListener(new CameraSurfaceView.OnPreviewListener() {
-            @Override
-            public void onPreviewFrame(byte[] data,int degree) {
-                if (!rgbOrIrConfirm) {
-                    rgbOrIr(0,mCameraSurfaceView1.mPreviewWidth, mCameraSurfaceView1.mPreviewHeight, data);
-                    if (rgbOrIrConfirm) {
-                        //要等两个摄像头都返回一帧数据，rgbOrIrConfirm才会被赋值，此时才能判断到底哪个是RGB摄像头
-                        choiceRgbOrIrType(0, data);
-                        if (camera1IsRgb) {
-                            mCameraSurfaceView1.setIr(false);
-                            btn_rgb.setText(R.string.rgb_capture);
-                            btn_ir.setText(R.string.ir_capture);
-                            Log.i(TAG, "mCameraSurfaceView1 is Rgb.mCameraSurfaceView2 is Ir.");
-                        } else {
-                            mCameraSurfaceView1.setIr(true);
-                            btn_rgb.setText(R.string.ir_capture);
-                            btn_ir.setText(R.string.rgb_capture);
-                            Log.i(TAG, "mCameraSurfaceView1 is Ir.mCameraSurfaceView2 is Rgb.");
-                        }
+        mCameraSurfaceView1.setOnPreviewListener((data, degree) -> {
+            if (!rgbOrIrConfirm) {
+                rgbOrIr(0,mCameraSurfaceView1.mPreviewWidth, mCameraSurfaceView1.mPreviewHeight, data);
+                if (rgbOrIrConfirm) {
+                    //要等两个摄像头都返回一帧数据，rgbOrIrConfirm才会被赋值，此时才能判断到底哪个是RGB摄像头
+                    choiceRgbOrIrType(0, data);
+                    if (camera1IsRgb) {
+                        mCameraSurfaceView1.setIr(false);
+                    } else {
+                        mCameraSurfaceView1.setIr(true);
                     }
                 }
-
             }
         });
 
 
-        mCameraSurfaceView2.setOnPreviewListener(new CameraSurfaceView.OnPreviewListener() {
-            @Override
-            public void onPreviewFrame(byte[] data,int degree) {
+        mCameraSurfaceView2.setOnPreviewListener((data, degree) -> {
 
-                if (!rgbOrIrConfirm) {
-                    rgbOrIr(1, mCameraSurfaceView2.mPreviewWidth, mCameraSurfaceView2.mPreviewHeight, data);
-                    if (rgbOrIrConfirm) {
-                        //要等两个摄像头都返回一帧数据，rgbOrIrConfirm才会被赋值，此时才能判断到底哪个是RGB摄像头
-                        choiceRgbOrIrType(1, data);
-                        if (camera1IsRgb) {
-                            mCameraSurfaceView2.setIr(true);
-                            btn_rgb.setText(R.string.ir_capture);
-                            btn_ir.setText(R.string.rgb_capture);
-                            Log.i(TAG, "mCameraSurfaceView1 is Ir.mCameraSurfaceView2 is Rgb.");
-                        } else {
-                            mCameraSurfaceView2.setIr(false);
-                            btn_rgb.setText(R.string.rgb_capture);
-                            btn_ir.setText(R.string.ir_capture);
-                            Log.i(TAG, "mCameraSurfaceView1 is Rgb.mCameraSurfaceView2 is Ir.");
-
-                        }
+            if (!rgbOrIrConfirm) {
+                rgbOrIr(1, mCameraSurfaceView2.mPreviewWidth, mCameraSurfaceView2.mPreviewHeight, data);
+                if (rgbOrIrConfirm) {
+                    //要等两个摄像头都返回一帧数据，rgbOrIrConfirm才会被赋值，此时才能判断到底哪个是RGB摄像头
+                    choiceRgbOrIrType(1, data);
+                    if (camera1IsRgb) {
+                        mCameraSurfaceView2.setIr(true);
+                    } else {
+                        mCameraSurfaceView2.setIr(false);
                     }
                 }
-
-
-                // 1. 在子线程中执行方法，小心内存泄露
-                executorService.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (isSafe()&& Boolean.FALSE.equals(FaceSearchEngine.Companion.getInstance().isProcessing())) {
-                            realTimeFaceBmp = BitmapUtils.convertPreviewFrameToBitmap(data, 640, 480);
-                            if(realTimeFaceBmp!=null){
-                                //自行保证Bitmap 的方向角度正确无旋转，清晰度，断点调试看看Bitmap
-                                FaceSearchEngine.Companion.getInstance().runSearch(adjustPhotoRotation(realTimeFaceBmp,-degree));
-                                Log.e(TAG, "getInstance().runSearch ---"+Thread.currentThread().getName()+ "Degree:"+ -degree);
-                            }
-                        }
-                    }
-                });
-
             }
+
+
+            // 1. 在子线程中执行方法，小心内存泄露
+            executorService.execute(() -> {
+                if (isSafe()&& Boolean.FALSE.equals(FaceSearchEngine.Companion.getInstance().isProcessing())) {
+                    realTimeFaceBmp = BitmapUtils.convertPreviewFrameToBitmap(data, 640, 480);
+                    if(realTimeFaceBmp!=null){
+
+                        realTimeFaceBmp=BitmapUtils.adjustPhotoRotation(realTimeFaceBmp,-degree);
+                        //自行保证Bitmap 的方向角度正确无旋转，清晰度，断点调试看看Bitmap
+                        FaceSearchEngine.Companion.getInstance().runSearch(realTimeFaceBmp);
+                        Log.e(TAG, "getInstance().runSearch ---"+Thread.currentThread().getName()+ "Degree:"+ -degree);
+                    }
+                }
+            });
+
         });
 
 
@@ -210,7 +182,6 @@ public class CameraActivity extends BaseActivity {
                     public void onProcessTips(int i) {
                         showPrecessTips(i);
                     }
-
                     //坐标框和对应的 搜索匹配到的图片标签
                     //人脸检测成功后画白框，此时还没有标签字段Label 字段为空
                     //人脸搜索匹配成功后白框变绿框，并标记出对应的Label
@@ -240,48 +211,6 @@ public class CameraActivity extends BaseActivity {
     }
 
 
-    Bitmap adjustPhotoRotation(Bitmap bm, final int orientationDegree) {
-        if(orientationDegree==0){
-            return bm;
-        }
-
-        Matrix m = new Matrix();
-        m.setRotate(orientationDegree, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
-        try {
-            Bitmap bm1 = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), m, true);
-            return bm1;
-        } catch (OutOfMemoryError ex) {
-        }
-        return null;
-    }
-
-
-    Bitmap adjustPhotoRotation2(Bitmap bm, final int orientationDegree) {
-        Matrix m = new Matrix();
-        m.setRotate(orientationDegree, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
-        float targetX, targetY;
-        if (orientationDegree == 90) {
-            targetX = bm.getHeight();
-            targetY = 0;
-        } else {
-            targetX = bm.getHeight();
-            targetY = bm.getWidth();
-        }
-
-        final float[] values = new float[9];
-        m.getValues(values);
-
-        float x1 = values[Matrix.MTRANS_X];
-        float y1 = values[Matrix.MTRANS_Y];
-
-        m.postTranslate(targetX - x1, targetY - y1);
-        Bitmap bm1 = Bitmap.createBitmap(bm.getHeight(), bm.getWidth(), Bitmap.Config.ARGB_8888);
-        Paint paint = new Paint();
-        Canvas canvas = new Canvas(bm1);
-        canvas.drawBitmap(bm, m, paint);
-
-        return bm1;
-    }
 
 
     /**
@@ -329,6 +258,8 @@ public class CameraActivity extends BaseActivity {
 
 
     /**
+     * 判断是RGB 摄像头还是双目摄像头
+     *
      * 参考：https://blog.csdn.net/yzzzza/article/details/107670521
      *
      * @param index
@@ -413,113 +344,19 @@ public class CameraActivity extends BaseActivity {
         }
         mCameraSurfaceView1.release();
         mCameraSurfaceView2.release();
-
-    }
-
-    public void btnRgbTakePhoto(View view) {
-        if (mCameraSurfaceView1 == null ||  mCameraSurfaceView1.mCamera == null) {
-            return;
-        }
-        mCameraSurfaceView1.mCamera.takePicture(null, null, new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-
-                bitmap = convertBmp(bitmap);
-                Matrix matrix = new Matrix();
-                matrix.setRotate(mCameraSurfaceView1.getPreviewDegree());
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-
-                bitmap = BitmapUtils.scaleBitmap(bitmap, 0.5f);
-
-                iv_rgb.setImageBitmap(bitmap);
-
-                mCameraSurfaceView1.startPreview();
-
-                saveImg(bitmap, "rgb.jpg");
-            }
-        });
     }
 
 
-    /**
-     * 红外
-     *
-     * @param view
-     */
-    public void btnIrTakePhoto(View view) {
-        if (mCameraSurfaceView2 == null ||  mCameraSurfaceView2.mCamera == null) {
-            Toast.makeText(this,"未找到摄像头",Toast.LENGTH_LONG).show();
-            return;
-        }
-        mCameraSurfaceView2.mCamera.takePicture(null, null, new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
-                Bitmap bitmap = convertBmp(bm);
-                Matrix matrix = new Matrix();
-                matrix.setRotate(mCameraSurfaceView2.getPreviewDegree());
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
-                bitmap = BitmapUtils.scaleBitmap(bitmap, 0.5f);
-                iv_ir.setImageBitmap(bitmap);
-                mCameraSurfaceView2.startPreview();
-                saveImg(bm, "ir.jpg");
-
-            }
-        });
-    }
-
-    public Bitmap convertBmp(Bitmap bmp) {
-        int w = bmp.getWidth();
-        int h = bmp.getHeight();
-
-        Matrix matrix = new Matrix();
-        matrix.postScale(-1, 1); // 镜像水平翻转
-        Bitmap convertBmp = Bitmap.createBitmap(bmp, 0, 0, w, h, matrix, true);
-
-        return convertBmp;
-    }
-
-    private void saveImg(Bitmap bm, String fileName) {
-        File dir = new File(Environment.getExternalStorageDirectory(), "temp");
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        File file = new File(dir, fileName);
-        BufferedOutputStream bos;
-        try {
-            bos = new BufferedOutputStream(new FileOutputStream(file));
-            bm.compress(Bitmap.CompressFormat.JPEG, 100, bos);
-            bos.flush();
-            bos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 把预览帧转为图片并保存
-     *
-     * @param data
-     * @param width
-     * @param height
-     * @param fileName
-     */
-    public void savePreviewFrameToBitmap(byte[] data, int width, int height, String fileName) {
-        Bitmap bitmap = BitmapUtils.convertPreviewFrameToBitmap(data, width, height);
-        saveImg(bitmap, fileName);
-    }
-
-    public void btnFaceDetect(View view) {
-        if (mCameraSurfaceView1 != null) {
-            mCameraSurfaceView1.startPreview();
-        }
-
-        if (mCameraSurfaceView2 != null) {
-            mCameraSurfaceView2.startPreview();
-        }
-    }
+//    public void btnFaceDetect(View view) {
+//        if (mCameraSurfaceView1 != null) {
+//            mCameraSurfaceView1.startPreview();
+//        }
+//
+//        if (mCameraSurfaceView2 != null) {
+//            mCameraSurfaceView2.startPreview();
+//        }
+//    }
 
     public void btnSetting(View view) {
         startActivity(new Intent(this,CameraSetting.class));
--- app/src/main/java/com/sjl/binocularcamera/util/BitmapUtils.java
+++ app/src/main/java/com/sjl/binocularcamera/util/BitmapUtils.java
@@ -2,17 +2,78 @@ package com.sjl.binocularcamera.util;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 
 import java.io.ByteArrayOutputStream;
 
 /**
- * 图片处理
+ * 图片Bitmap的处理
  */
 public class BitmapUtils {
+
+    /**
+     * 调整Bitmap 的角度
+     *
+     * @param bm
+     * @param orientationDegree
+     * @return
+     */
+    public static Bitmap adjustPhotoRotation(Bitmap bm, final int orientationDegree) {
+        if (orientationDegree == 0) {
+            return bm;
+        }
+
+        Matrix m = new Matrix();
+        m.setRotate(orientationDegree, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        try {
+            Bitmap bm1 = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), m, true);
+            return bm1;
+        } catch (OutOfMemoryError ex) {
+        }
+        return null;
+    }
+
+
+    /**
+     * 效率高？用户自己调试吧
+     *
+     * @param bm
+     * @param orientationDegree
+     * @return
+     */
+    public static Bitmap adjustPhotoRotation2(Bitmap bm, final int orientationDegree) {
+        Matrix m = new Matrix();
+        m.setRotate(orientationDegree, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        float targetX, targetY;
+        if (orientationDegree == 90) {
+            targetX = bm.getHeight();
+            targetY = 0;
+        } else {
+            targetX = bm.getHeight();
+            targetY = bm.getWidth();
+        }
+
+        final float[] values = new float[9];
+        m.getValues(values);
+
+        float x1 = values[Matrix.MTRANS_X];
+        float y1 = values[Matrix.MTRANS_Y];
+
+        m.postTranslate(targetX - x1, targetY - y1);
+        Bitmap bm1 = Bitmap.createBitmap(bm.getHeight(), bm.getWidth(), Bitmap.Config.ARGB_8888);
+        Paint paint = new Paint();
+        Canvas canvas = new Canvas(bm1);
+        canvas.drawBitmap(bm, m, paint);
+
+        return bm1;
+    }
+
+
     /**
      * 按比例缩放图片
      *
@@ -35,6 +96,7 @@ public class BitmapUtils {
         origin.recycle();
         return newBM;
     }
+
     /**
      * 把预览帧转为Bitmap，测试YUV是否能正常转化好
      *
